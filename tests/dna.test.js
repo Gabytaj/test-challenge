e50a728e016dcfa065ff529325b351cd416ca8b7
GabyJ <gabytaj26@gmail.com>
2023-03-22 15:02:43 -0600

    correccion test

--- tests/dna.test.js
+++ tests/dna.test.js
@@ -2,6 +2,6 @@ const { default: expect } = require('expect');
 const DNAString = require('../dna');
 
 test('DNAPalabra', () => {
-    let actual = DNAString('GABYvvhhjJAC');
-    expect(actual).toBe('GABYJAC');
+    let actual = DNAString('acgtCTAGGGcTAratataA');
+    expect(actual).toBe('CTAGGGTAA');
 })
